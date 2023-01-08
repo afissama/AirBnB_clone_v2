cee2fec41b018e16087a179e514493db4b28aa6a
afis <mahoutinafis@gmail.com>
2023-01-08 21:37:29 +0100

    setup test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,7 @@
+-- Prepare the MYSQL server for the project
+-- hbnb_test_db will be created
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
